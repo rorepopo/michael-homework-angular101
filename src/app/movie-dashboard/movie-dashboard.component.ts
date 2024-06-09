e3ff3feecb0055566f5223ec8a0a9e7a6bee55ab
rorepopo <117485835+rorepopo@users.noreply.github.com>
2024-06-08 20:32:34 -0400

    start app

---
+++ src/app/movie-dashboard/movie-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-dashboard.component.html',
+  styleUrl: './movie-dashboard.component.css'
+})
+export class MovieDashboardComponent {
+
+}
